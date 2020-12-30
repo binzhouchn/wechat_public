3aa7ba68acdb9c400dc6f374f93e6c36c6943134
binzhouchn <binzhouchn@gmail.com>
2020-12-30 17:05:51 +0800

    增加图片识别功能

--- Semantic/img_clf.py
+++ Semantic/img_clf.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 ## 用百度SDK（安装aip）
 from aip import AipImageClassify
+import requests
 import urllib
 from config import APP_ID, API_KEY, SECRET_KEY
 # [SDK文档地址](https://cloud.baidu.com/doc/IMAGERECOGNITION/s/4k3bcxj1m)
@@ -13,11 +14,15 @@ class ImageCLF:
         self.client.setSocketTimeoutInMillis(8000)
     def image_classif(self, wechat_img_url, baike_num=0):
         print('wechat_img_url: ', wechat_img_url)
-        pic_name = 'z_' + wechat_img_url.replace('https://', '').replace('http://', '').replace('/', '').replace('.jpg','').replace(
-            '.jpeg', '').replace('.png', '').replace('.', '')[-6:] + '.png'
-        save_path = '~/Downloads/{}'.format(pic_name)
-        urllib.request.urlretrieve(wechat_img_url, save_path)
-        image = self.get_file_content(save_path)
+        # # 图片save到本地
+        # pic_name = 'z_' + wechat_img_url.replace('https://', '').replace('http://', '').replace('/', '').replace('.jpg','').replace(
+        #     '.jpeg', '').replace('.png', '').replace('.', '')[-6:] + '.png'
+        # save_path = '~/Downloads/{}'.format(pic_name)
+        # urllib.request.urlretrieve(wechat_img_url, save_path)
+        # image = self.get_file_content(save_path)
+
+        # 图片不save本地，直接request.get成bytes
+        image = requests.get(wechat_img_url).content
         """ 如果有可选参数 """
         options = {}
         options["baike_num"] = baike_num #百度百科数量，默认返回0
@@ -38,16 +43,13 @@ imageclf = ImageCLF()
 # import urllib
 # from config import ACCESS_TOKEN
 #
-# # 直接调用接口，只需要提供access_token
+# # 直接调用接口，所以暂时不写类了
 # def image_classif(wechat_img_url):
-#     pic_name = 'z_'+ wechat_img_url.replace('https://','').replace('http://','').replace('/','').replace('.jpg','').replace('.jpeg','').replace('.png','').replace('.','')[-6:]+ '.png'
-#     save_path = '~/Downloads/{}'.format(pic_name)
-#     urllib.request.urlretrieve(wechat_img_url, save_path)
+#     # 需要调用的百度接口url
 #     request_url = "https://aip.baidubce.com/rest/2.0/image-classify/v2/advanced_general"
-#     # 二进制方式打开图片文件
-#     f = open(save_path, 'rb')
-#     img = base64.b64encode(f.read())
-#     params = {"image": img}
+#     print('wechat_img_url: ',wechat_img_url)
+#     image = base64.b64encode(requests.get(wechat_img_url).content)
+#     params = {"image": image}
 #     access_token = ACCESS_TOKEN
 #     request_url = request_url + "?access_token=" + access_token
 #     headers = {'content-type': 'application/x-www-form-urlencoded'}
